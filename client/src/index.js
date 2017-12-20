179427e1c5ade89300c0f1337caeed9c56417c1a
Daniel Roberts <BeardOfDan@gmail.com>
2017-12-20 11:08:44 -0800

    (feat) merged index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './components/App.jsx';
+import App from './components/App';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>, 
-  root);
+  </BrowserRouter>,
+  root
+);
